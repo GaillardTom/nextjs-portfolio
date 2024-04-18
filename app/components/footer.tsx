316ffb1c231a9415a8da4f08c24a600ac05b7673
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2024-04-18 17:15:10 -0400

    Added fixed width to footer and fixed positioning now responsive too

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -14,18 +14,18 @@ export function Footer() {
     const [visible, setVisible] = useState(false);
 
   return (
-    <aside className="sm:fixed relative bottom-0 justify-center content-center items-center w-auto sm:right-8  tracking-tight">
-      <div className="lg:sticky md:bot-15 lg:bot-20">
+    <aside className="-mr-[55px] md:fixed relative bottom-0 sm:right-2 right-0 md:right-0 tracking-tight">
+      <div className="lg:sticky md:bot-15 lg:bot-20 md:w-48 md:h-64 lg:w-64">
 
         <nav
-          className="  items-start px-0 pb-0 rounded-lg md:overflow-auto md:relative"
+          className=" items-start px-0 pb-0  rounded-lg md:overflow-auto md:relative"
           id="nav"
         >
 
-<div className="flex flex-col-reverse space-x-0 pr-10 group mt-16">
+<div className="flex flex-col-reverse space-x-0 group mt-16 md:h-64 md:w-64">
 
 
-        <h1 className="sm:collapse prose-headings visible text-center item-center  align-middle justify-center text-xl font-semibold mb-5 mt-6">Contacts</h1>
+        <h1 className="md:collapse prose-headings visible text-center item-center  align-middle justify-center text-xl font-semibold mb-5 mt-6">Contacts</h1>
     <button onClick={() => setVisible(!visible)} className="z-1 group border-gray-600 border-solid border-2 text-lg text-center footer-circle align-middle items-center content-center self-center justify-center">
 
                 <div className="flex flex-col">
@@ -33,7 +33,7 @@ export function Footer() {
 
                     
                 <Image width={75} height={75} alt="Contacts" src="/images/address-book.png" className='group-hover:animate-pulse' />
-                {/* <h1 className="text-sm prose prose-neutral dark:prose-invert group-hover:animate-pulse font-bold">Contacts</h1> */}
+                {/* <h1 className="text-md prose prose-neutral dark:prose-invert group-hover:animate-pulse font-bold">Contacts</h1> */}
                 </div> 
             </button>
 
@@ -46,7 +46,8 @@ export function Footer() {
                 <Link
                   key={social}
                   href={link}
-                  className=" z-1 text-xl transition-all hover:text-neutral-800 dark:hover:text-neutral-200  align-middle items-center content-center relative py-1 px-2 focus:bg-green-900"
+                  target='_blank'
+                  className=" z-1 text-xl transition-all hover:text-neutral-800 dark:hover:text-neutral-200  align-middle items-center content-center relative py-1 px-2"
                 >
                     <Image className='relative fade-in hover:-translate-y-1 mb-5' alt="test" width={30} height={30} src={"/images/"+social+".jpg"}></Image>
                 </Link>
