9f593c363d7399f47970c1bb174d6fc256cbca16
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2024-04-19 18:54:01 -0400

    Trying to fix bug with logo

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots() {
         userAgent: '*',
       },
     ],
-    sitemap: 'https://leerob.io/sitemap.xml',
-    host: 'https://leerob.io',
+    sitemap: 'https://tom-gaillard.vercel.app/sitemap.xml',
+    host: 'https://tom-gaillard.vercel.app',
   };
 }
