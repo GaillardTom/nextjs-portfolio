1f934a45c712d74a47dd93c2b4bbfec05b2dcb50
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2024-04-20 21:09:14 -0400

    Added some animation again and modified the rest check notion, ready to go pub waiting for test and merge

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -24,16 +24,16 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row max-h-28 min-h-14  items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row max-h-28 min-h-14 items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row space-x-0 pr-10  md:gap-2">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="text-xl transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 focus:bg-green-900 rounded-lg"
+                  className="wave-effect text-xl transition-all hover:scale-105 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 rounded-lg"
                 >
                   {name}
                 </Link>
