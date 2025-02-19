1bc15420e472c866baed0d71c30d701c5b34949a
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2025-02-19 15:37:47 -0500

    Added the GameNightPokerAI blog post with images and videos looks really cool imo

--- app/components/tags.tsx
+++ app/components/tags.tsx
@@ -3,9 +3,9 @@ import react from 'react';
 
 export function Tags({tagsDict, redirect, selectedTag}) {
   return (
-    <div className="collapse group md:visible lg:visible flex-wrap grid grid-cols-2 gap-2 fixed lg:left-44 md:top-16 right-5 lg:right-auto w-32 lg:w-auto lg:top-auto border-b-2 border-gray-500 md:right-20">
+    <div className="collapse group  md:invisible lg:visible flex-wrap grid grid-cols-2 gap-2 fixed lg:left-44 md:top-16 right-5 lg:right-auto w-32 lg:w-auto lg:top-auto border-b-2 border-gray-500 md:right-20">
         <h1 className="font-bold text-2xl tracking-tighter w-16 lg:w-32 text-center md:border-solid md:border-2 lg:border-gray-500 md:rounded-full ">Tags</h1>
-        <div className='invisible md:visible lg:visible  border-b-2 border-gray-500 mb-4 -ml-[6%]'></div>
+        <div className='invisible  lg:visible  border-b-2 border-gray-500 mb-4 -ml-[6%]'></div>
     {Object.entries(tagsDict).map(([tag, count]) => (
        tag === selectedTag ?  <Link
             key={tag}
