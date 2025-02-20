270b5b494d27d535199fa5a5098dedcd1fc19c46
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2025-02-20 17:17:41 -0500

    Fixed a bit the responsive design of the tags still some work to do, but it's better now

--- app/components/tags.tsx
+++ app/components/tags.tsx
@@ -5,15 +5,13 @@ export function Tags({tagsDict, redirect, selectedTag}) {
   //SImple fix for the tags now if already selected it will deselect it
   const selectedRedirect = redirect === '/blogtags/' ? '/blog/' : '/writeups/';
   return (
-    <div className="collapse group  md:invisible lg:visible flex-wrap grid grid-cols-2 gap-2 fixed lg:left-44 md:top-16 right-5 lg:right-auto w-32 lg:w-auto lg:top-auto border-b-2 border-gray-500 md:right-20">
-        <h1 className="font-bold text-2xl tracking-tighter w-16 lg:w-32 text-center md:border-solid md:border-2 lg:border-gray-500 md:rounded-full ">Tags</h1>
-        <div className='invisible  lg:visible  border-b-2 border-gray-500 mb-4 -ml-[6%]'></div>
+    <div className="invisible md:visible group left-2 md:right-4 md:left-auto bottom-32 md:bottom-auto md:top-16 sm:top-auto w-fit  xl:visible flex-wrap grid grid-cols-2 gap-2 fixed 2xl:left-32  2xl:right-auto   2xl:w-auto 2xl:top-auto border-b-2 border-gray-500 ">
+        <h1 className=" font-bold text-2xl tracking-tighter w-32 lg:w-32 text-center border-solid border-2 border-gray-500 rounded-full ">Tags</h1>
+        <div className='border-b-2 border-gray-500 mb-4 -ml-[6%]'></div>
     {Object.entries(tagsDict).map(([tag, count]) => (
        tag === selectedTag ?   <Link
             
        key={tag}
-            //href = {/blog/} if the redirect is blogtag else its /writeups/
-
             href={selectedRedirect}
             className="mb-8 btn  text-md text-centers text-neutral-600 dark:text-neutral-100 hover:text-neutral-800 dark:hover:text-neutral-300 px-1 py-1 rounded-lg transition-all duration-200 ease-in-out hover:translate-y-1 hover:border-opacity-0"
             
@@ -28,7 +26,7 @@ export function Tags({tagsDict, redirect, selectedTag}) {
         <Link
             key={tag}
             href={`${redirect}${tag}`}
-            className="group-hover:visible invisible -mb-8  group-hover:mb-8 wave-effect w-32 text-md text-centers text-neutral-600 dark:text-neutral-100 hover:text-neutral-800 dark:hover:text-neutral-300 px-1 py-1 rounded-lg transition-all duration-200 ease-in-out hover:translate-y-1 hover:border-opacity-0"
+            className="group-hover:mb-8 wave-effect w-32 text-md text-center justify-items-center text-neutral-600 dark:text-neutral-100 hover:text-neutral-800 dark:hover:text-neutral-300 px-1 py-1 rounded-lg transition-all duration-200 ease-in-out hover:translate-y-1 hover:border-opacity-0"
             
         >
               <div className="flex flex-row text-center">
